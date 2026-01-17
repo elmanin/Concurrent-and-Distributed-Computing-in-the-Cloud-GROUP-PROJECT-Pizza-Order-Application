a68c21318ba5e1560c02059c6149b6ac8ca3f5ac
elmanin <alu.156518@usj.es>
2026-01-17 11:16:11 +0100

    didn't work

--- src/app/people/people.repository.js
+++ src/app/people/people.repository.js
@@ -3,14 +3,10 @@ const bcrypt = require("bcrypt");
 
 async function createUser(userData) {
   try {
-    const query = `INSERT INTO people(email, password, name, lastname, role) VALUES (:email, :password, :name, :lastname, :role)`;
-    await connection.performAsyncQuery(query, {
-      email: userData.email,
-      password: userData.password,
-      name: userData.name,
-      lastname: userData.lastname,
-      role: userData.role,
-    });
+    const connection = new DBConnector();
+    const query = `INSERT INTO people(email, password, name, lastname, role) 
+                   VALUES ('${userData.email}', '${userData.password}', '${userData.name}', '${userData.lastname}', '${userData.role}')`;
+    await connection.performAsyncQuery(query);
   } catch (err) {
     console.error(err);
     throw err;
@@ -21,10 +17,8 @@ async function getUserByEmail(email) {
   try {
     const connection = new DBConnector();
     const user = await connection.performAsyncQuery(
-      "SELECT * FROM people WHERE email = :email",
-      { email },
+      `SELECT * FROM people WHERE email='${email}'`,
     );
-
     return user;
   } catch (err) {
     console.error(err);
